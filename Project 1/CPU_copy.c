dc0eb574b87cd49f7d4c9b10140c330b00012290
Patrick Lyons <plyons72@gmail.com>
2017-10-02 18:36:54 -0400

    Corrected syntax errors so that program compiles correctly

--- Project 1/CPU_copy.c
+++ Project 1/CPU_copy.c
@@ -4,15 +4,15 @@
  Patrick Lyons
  just compile with gcc -o CPU CPU.c
  and execute using
- ./CPU 'trace file' 'prediction_method' 'trace_vew_on'
+ ./CPU 'trace file' 'prediction_method' 'trace_view_on'
  ***************************************************************/
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
 #include <arpa/inet.h>
 #include "CPU.h"
 
-char stage_id_map(int i);
+char *stage_id_map(int i);
 
 int main(int argc, char **argv)
 {
@@ -20,29 +20,29 @@ int main(int argc, char **argv)
     //struct trace_item tr_entry;
     size_t size; //
     char *trace_file_name;
-    int prediciton_method = 0; //branch prediction on or off. 0 for predict not taken, 1 for 1-bit prediction. Default value is 0
+    int prediction_method = 0; //branch prediction on or off. 0 for predict not taken, 1 for 1-bit prediction. Default value is 0
     int trace_view_on = 0; //Cycle output printed to screen on or off. 0 for off, 1 for on
-    
+
     unsigned char t_type = 0;
     unsigned char t_sReg_a= 0;
     unsigned char t_sReg_b= 0;
     unsigned char t_dReg= 0;
     unsigned int t_PC = 0;
     unsigned int t_Addr = 0;
-    
+
     unsigned int cycle_number = 0;
-    
+
     if (argc == 1) { //If the input does not include the filename there is nothing to trace (i.e. no input), so exit
         fprintf(stdout, "\nUSAGE: tv <trace_file> <switch - any character>\n");
         fprintf(stdout, "\n(switch) to turn on or off individual item view.\n\n");
         exit(0);
     }
-    
-    if (arcc > 4) {
+
+    if (argc > 4) {
         fprintf(stdout, "\nToo many arguments innput\n\n");
         exit(0);
     }
-    
+
     trace_file_name = argv[1]; //filename is the second argument, so take it
     if (argc == 3) {
         prediction_method = atoi(argv[2]); //prediction_method will be changed from defualt if it is included. It is the third argument
@@ -53,26 +53,26 @@ int main(int argc, char **argv)
     }else if (argc == 4) {
         prediction_method = atoi(argv[2]); //prediction_method will be changed from defualt if it is included. It is the third argument
         trace_view_on = atoi(argv[3]) ; //trace_view_on will be changed from defualt if it is included. It is the fourth argument
-        if (prediction_method < 0 || prediction_method > 1 || trace_view_on < 0 || trace_vew_on > 1) {
+        if (prediction_method < 0 || prediction_method > 1 || trace_view_on < 0 || trace_view_on > 1) {
             fprintf(stdout, "\nInvalid input arguments\n\n");
             exit(0);
         }
     }
-    
+
     fprintf(stdout, "\n ** opening file %s\n", trace_file_name);
-    
+
     trace_fd = fopen(trace_file_name, "rb"); //open file to be read
-    
+
     if (!trace_fd) { //if file fopen() returns an error, exit
         fprintf(stdout, "\ntrace file %s not opened.\n\n", trace_file_name);
         exit(0);
     }
-    
+
     trace_init(); //initialize the trace
-    
+
     while(1) { //Execute the trace program
         size = trace_get_item(&tr_entry); //get size of the trace program as well as the trace_item field
-        
+
         if (!size) {       /* no more instructions (trace_items) to simulate */
             printf("+ Simulation terminates at cycle : %u\n", cycle_number);
             break;
@@ -86,11 +86,11 @@ int main(int argc, char **argv)
             t_PC = tr_entry->PC;
             t_Addr = tr_entry->Addr;
         }
-        
+
         // SIMULATION OF A Pipelined CPU
-        
+
         if (trace_view_on) {/* print the executed instruction if trace_view_on=1 */
-            
+
             //int i;
             //for(i = 5; i > 0; i--){
             /*switch (i) {
@@ -132,9 +132,9 @@ int main(int argc, char **argv)
                     break;
             }
             /*break;
-             
+
              default:
-             
+
              switch(tr_entry[i]->type) {
              case ti_NOP:
              printf("NOP:") ;
@@ -176,14 +176,14 @@ int main(int argc, char **argv)
         }
         //}
     }
-        
+
     trace_uninit(); //uninitialize the trace and exit
-    
+
     exit(0);
 }
 
-char stage_id_map(int i) {
-    
+char *stage_id_map(int i) {
+
     if(i == 5) {
         return "IF";
     }else if(i == 4) {
@@ -195,9 +195,5 @@ char stage_id_map(int i) {
     }else {
         return "WB";
     }
-    
+
 }
-
-
-
-
