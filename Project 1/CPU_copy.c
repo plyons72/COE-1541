65526ef8d3546307cc0573626b27d5cfe0eb827f
Patrick Lyons <plyons72@gmail.com>
2017-10-03 16:38:53 -0400

    Added minor comment

--- Project 1/CPU_copy.c
+++ Project 1/CPU_copy.c
@@ -24,27 +24,27 @@ int main(int argc, char **argv)
     char *trace_file_name;
     int prediction_method = 0; //branch prediction on or off. 0 for predict not taken, 1 for 1-bit prediction. Default value is 0
     int trace_view_on = 0; //Cycle output printed to screen on or off. 0 for off, 1 for on
-    
+
     unsigned char t_type = 0;
     unsigned char t_sReg_a= 0;
     unsigned char t_sReg_b= 0;
     unsigned char t_dReg= 0;
     unsigned int t_PC = 0;
     unsigned int t_Addr = 0;
-    
+
     unsigned int cycle_number = 0;
-    
+
     if (argc == 1) { //If the input does not include the filename there is nothing to trace (i.e. no input), so exit
         fprintf(stdout, "\nUSAGE: tv <trace_file> <switch - any character>\n");
         fprintf(stdout, "\n(switch) to turn on or off individual item view.\n\n");
         exit(0);
     }
-    
+
     if (argc > 4) {
         fprintf(stdout, "\nToo many arguments innput\n\n");
         exit(0);
     }
-    
+
     trace_file_name = argv[1]; //filename is the second argument, so take it
     if (argc == 3) {
         prediction_method = atoi(argv[2]); //prediction_method will be changed from defualt if it is included. It is the third argument
@@ -60,21 +60,21 @@ int main(int argc, char **argv)
             exit(0);
         }
     }
-    
+
     fprintf(stdout, "\n ** opening file %s\n", trace_file_name);
-    
+
     trace_fd = fopen(trace_file_name, "rb"); //open file to be read
-    
+
     if (!trace_fd) { //if file fopen() returns an error, exit
         fprintf(stdout, "\ntrace file %s not opened.\n\n", trace_file_name);
         exit(0);
     }
-    
+
     trace_init(); //initialize the trace
-    
+
     while(1) { //Execute the trace program
         size = trace_get_item(&tr_entry); //get size of the trace program as well as the trace_item field
-        
+
         if (!size && checkPipeline(&tr_entry)) {       /* no more instructions (trace_items) to simulate */
             printf("+ Simulation terminates at cycle : %u\n", cycle_number);
             break;
@@ -88,11 +88,11 @@ int main(int argc, char **argv)
             t_PC = tr_entry->PC;
             t_Addr = tr_entry->Addr;
         }
-        
+
         // SIMULATION OF A Pipelined CPU
-        
+
         if (trace_view_on) {/* print the executed instruction if trace_view_on=1 */
-            
+
             int i;
             for(i = 0; i > -5; i--){
                 char *temp = stage_ID_map(i);
@@ -177,14 +177,14 @@ int main(int argc, char **argv)
             }
         }
     }
-    
+
     trace_uninit(); //uninitialize the trace and exit
-    
+
     exit(0);
 }
 
 char *stage_ID_map(int i) {
-    
+
     if(i == 0) {
         return "IF";
     }else if(i == -1) {
@@ -196,14 +196,16 @@ char *stage_ID_map(int i) {
     }else {
         return "WB";
     }
-    
+
 }
 
+
+//Checks to see if there are unfinished traces in the pipeline
 int checkPipeline(struct trace_item **item) {
-    
-    
+
+
     int x = 0;
-    
+
     int j;
     for(j = 0; j > -5; j--) {
         switch((*item)[j].type) {
@@ -213,21 +215,17 @@ int checkPipeline(struct trace_item **item) {
         }
 
     }
-    
+
     for(j = -4; j < 0; j++){
         (*item)[j] = (*item)[j+1];
     }
     (*item)[0].type = 0;
-    
+
     if (x == 5) {
-        
+
         return 1;
     }
-    
+
     return 0;
-    
+
 }
-
-
-
-
