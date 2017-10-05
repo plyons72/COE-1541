a0c94c90608b1d445383be0ee1d2cdbc6af38bc8
Patrick Lyons <plyons72@gmail.com>
2017-10-04 20:52:57 -0400

    Fixed syntactical errors. Code compiles

--- Project 1/CPU1.c
+++ Project 1/CPU1.c
@@ -17,13 +17,13 @@ char *stage_ID_map(int i);
 int checkPipeline(struct trace_item **item);
 void insert_NOP(struct trace_item **item, int stage);
 void shift(struct trace_item **item);
-int check_data_hazard();
+int check_data_hazard(struct trace_item **item);
 struct trace_item temp;
 
 
 int main(int argc, char **argv)
 {
-    struct trace_item *tr_entry; //create a new trace_item struct pointer
+    struct trace_item *item; //create a new trace_item struct pointer
     struct trace_item *tr_pipeline;
     size_t size;
     char *trace_file_name;
@@ -89,7 +89,7 @@ int main(int argc, char **argv)
     }
 
     while(1) { //Execute the trace program
-        size = trace_get_item(&tr_entry); //get size of the trace program as well as the trace_item field
+        size = trace_get_item(&item); //get size of the trace program as well as the trace_item field
 
         if (!size && !checkPipeline(&tr_pipeline)) {       /* no more instructions (trace_items) to simulate */
             printf("+ Simulation terminates at cycle : %u\n", cycle_number);
@@ -97,23 +97,22 @@ int main(int argc, char **argv)
         }
         else{              /* parse the next instruction to simulate */
             cycle_number++;
-            /*t_type = tr_entry->type;
-            t_sReg_a = tr_entry->sReg_a;
-            t_sReg_b = tr_entry->sReg_b;
-            t_dReg = tr_entry->dReg;
-            t_PC = tr_entry->PC;
-            t_Addr = tr_entry->Addr;*/
+            /*t_type = item->type;
+            t_sReg_a = item->sReg_a;
+            t_sReg_b = item->sReg_b;
+            t_dReg = item->dReg;
+            t_PC = item->PC;
+            t_Addr = item->Addr;*/
             shift(&tr_pipeline);
             if (!size) { //insert into pipeline
                 insert_NOP(&tr_pipeline, 0);
             }else {
-                tr_pipeline[0] = *tr_entry;
+                tr_pipeline[0] = *item;
             }
 
-            if (check_data_hazard())
+            if (check_data_hazard(&tr_pipeline))
             {
               insert_NOP(&tr_pipeline, 1);
-              i
             }
 
             //see if branch or jump first
@@ -316,22 +315,22 @@ void shift(struct trace_item **item) {
   the next one, and checking to see if a load instruction will affect something
   that follows it by changing the source of the next instruction.
 */
-int check_data_hazard(){
+int check_data_hazard(struct trace_item **item){
 
   /*
     If the next instruction is an R-Type instruction and the current is a load
     instruction, where the source of the next = the destination of the current,
     stall.
   */
-  if (tr_entry[0].type == 1 && tr_entry[1].type == 3) {
-    if (tr_entry->sReg_a == buffer[0].dReg) {
+  if (item[0]->type == 1 && item[1]->type == 3) {
+    if (item[0]->sReg_a == item[1]->dReg) {
       return 1;
     }
     /*
       Otherwise, if the second source of the next operation is the destination
       register of the previous operation, stall.
     */
-    else if (tr_entry->sReg_b == buffer[1].dReg) {
+    else if (item[0]->sReg_b == item[1]->dReg) {
     return 1;
     }
   }
@@ -341,8 +340,8 @@ int check_data_hazard(){
     instruction, AND the source register of the I-Type is the destination
     register of the Load instruction, return 1 to insert no-ops and stall.
   */
-  else if (tr_entry[0].type == 2 && tr_entry[1].type == 3) {
-    if (tr_entry->sReg_a == buffer[0].dReg) {
+  else if (item[0]->type == 2 && item[1]->type == 3) {
+    if (item[0]->sReg_a == item[1]->dReg) {
       return 1;
     }
   }
@@ -351,25 +350,32 @@ int check_data_hazard(){
     A store instruction follows a load instruction, where the source relies
     on the previous destination.
   */
-  else if (tr_entry[0].type == 4 && tr_entry[1].type == 3)
-    if (tr_entry->sReg_a == buffer[0].dReg)
+  else if (item[0]->type == 4 && item[1]->type == 3) {
+    if (item[0]->sReg_a == item[1]->dReg) {
       return 1;
+    }
+  }
 
   /*
     A branch instruction follows a load instruction where the branch relies on
     the value of the load word destination
   */
-  else if (tr_entry[0].type == 5 && tr_entry[1].type == 3){
-    if (tr_entry->sReg_a == buffer[0].dReg)
+  else if (item[0]->type == 5 && item[1]->type == 3){
+    if (item[0]->sReg_a == item[1]->dReg) {
       return 1;
+    }
 
-    else if (tr_entry->sReg_b == buffer[0].dReg)
+    else if (item[0]->sReg_b == item[1]->dReg) {
       return 1;
+    }
   }
 
   /*
     If none of these cases are met, there is no data hazard. Return 0 so that
     no stall takes place
   */
+  else { return 0; }
+
+  //Fallthrough case shouldn't be reached
   return 0;
 }
